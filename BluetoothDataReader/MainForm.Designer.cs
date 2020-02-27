28b21c48e1b46010fe645f388beff1f6a02ce05e
Emre <emreincekara@hotmail.com.tr>
2020-02-27 21:56:17 +0300

    Add project files.

---
+++ BluetoothDataReader/MainForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace BluetoothDataReader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cmbPorts = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblState = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmbPorts
+            // 
+            this.cmbPorts.FormattingEnabled = true;
+            this.cmbPorts.Location = new System.Drawing.Point(45, 17);
+            this.cmbPorts.Name = "cmbPorts";
+            this.cmbPorts.Size = new System.Drawing.Size(121, 21);
+            this.cmbPorts.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblState);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnConn);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cmbPorts);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(448, 49);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bağlantı";
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(313, 21);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(44, 13);
+            this.lblState.TabIndex = 3;
+            this.lblState.Text = "Bekliyor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(263, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Durum :";
+            // 
+            // btnConn
+            // 
+            this.btnConn.Location = new System.Drawing.Point(178, 16);
+            this.btnConn.Name = "btnConn";
+            this.btnConn.Size = new System.Drawing.Size(75, 23);
+            this.btnConn.TabIndex = 1;
+            this.btnConn.Text = "Bağlan";
+            this.btnConn.UseVisualStyleBackColor = true;
+            this.btnConn.Click += new System.EventHandler(this.btnConn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port :";
+            // 
+            // serialPort
+            // 
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(12, 68);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMessage.Size = new System.Drawing.Size(448, 231);
+            this.txtMessage.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 309);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtMessage);
+            this.Name = "MainForm";
+            this.Text = "Main";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbPorts;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConn;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.TextBox txtMessage;
+    }
+}
+
---
+++ BluetoothDataReader/MainForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace BluetoothDataReader
+{
+    public partial class MainForm : Form
+    {
+        private string recivedData;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+                cmbPorts.Items.Add(port);
+        }
+
+        private void serialPort_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            recivedData = serialPort.ReadLine();
+            this.Invoke(new EventHandler(displayData_Event));
+        }
+
+        private void displayData_Event(object sender, EventArgs e)
+        {
+            txtMessage.Text += recivedData + "\n";
+        }
+
+        private void btnConn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(cmbPorts.Text)) return;
+                if (!serialPort.IsOpen)
+                {
+                    serialPort.PortName = cmbPorts.Text;
+                    serialPort.BaudRate = 9600;
+                    serialPort.Open();
+                    btnConn.Text = "Kes";
+                    lblState.Text = "Bağlandı";
+                    lblState.ForeColor = System.Drawing.Color.Green;
+                }
+                else
+                {
+                    lblState.Text = "Bağlantı Kesildi";
+                    lblState.ForeColor = System.Drawing.Color.Red;
+                    btnConn.Text = "Bağlan";
+                    serialPort.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort.IsOpen) serialPort.Close();
+        }
+    }
+}
